5cb5e48c575c70ce694f72e305e264432ad4acfd
github username <github email address>
2019-05-14 16:06:49 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,56 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets 
 end
 
-def end_game
-  # code #end_game here
+def end_game (card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_1 = deal_card
+  card_2 = deal_card
+  display_card_total(card_1 + card_2)
+  card_1 + card_2
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  hit_or_stay = get_user_input
+  if 
+    hit_or_stay == "h"
+    card_3 = deal_card
+    card_total + card_3
+  elsif
+     hit_or_stay == "s"
+    card_total
+  else 
+    invalid_command
+  end
 end
+
+
 
 def invalid_command
-  # code invalid_command here
+  if get_user_input != "h" || get_user_input != 's'
+    "Please enter a valid command"
+    prompt_user
+  end
 end
 
 #####################################################
@@ -39,6 +58,17 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  card_total = initial_round
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game
 end
+
+
+
+
+
     
